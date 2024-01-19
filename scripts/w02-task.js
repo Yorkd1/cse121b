13cb441d12e5635bcaf99fac361b2e85aea2961d
Yorkd1 <davidgyork@gmail.com>
2024-01-18 18:40:58 -0700

    Added files, and modified W02-task to show content.

---
+++ scripts/w02-task.js
@@ -0,0 +1,41 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "David York";
+let currentYear = '2024';
+let profilePicture = '/images/IMG_1022.png';
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+
+
+
+
+/* Step 5 - Array */
+let food = ['Pizza', 'Tacos', 'Pasta', 'BBQ'];
+let otherFood = 'Sushi'
+food.push(otherFood);
+foodElement.innerHTML += `<br>${food}`;
+food.shift();
+foodElement.innerHTML += `<br>${food}`;
+food.pop();
+foodElement.innerHTML += `<br>${food}`;
+
+
+
+
