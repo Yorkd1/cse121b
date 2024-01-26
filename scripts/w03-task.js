d63acc6a2df0f3b763313a7812775a07699ef4ea
Yorkd1 <davidgyork@gmail.com>
2024-01-25 17:23:45 -0700

    Changes made to W03 assignment.

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -5,20 +5,17 @@
 function add(number1, number2) {
     return number1 + number2;
 }
-
 function addNumbers() {
     let addNumber1 = Number(document.querySelector('#add1').value);
     let addNumber2 = Number(document.querySelector('#add2').value);
     document.querySelector('#sum').value = add(addNumber1, addNumber2);
 }
-
 document.querySelector('#addNumbers').addEventListener('click', addNumbers);
 
 /* Function Expression - Subtract Numbers */
 function subtract(number1, number2) {
     return number1 - number2;
 }
-
 function subtractNumbers() {
     let subtractNumber1 = Number(document.querySelector('#subtract1').value);
     let subtractNumber2 = Number(document.querySelector('#subtract2').value);
@@ -29,7 +26,6 @@ document.querySelector('#subtractNumbers').addEventListener('click', subtractNum
 
 /* Arrow Function - Multiply Numbers */
 const multiply = (number1, number2) => number1 * number2;
-
 const multiplyNumbers = () => {
     let multiplyNumber1 = Number(document.querySelector('#factor1').value);
     let multiplyNumber2 = Number(document.querySelector('#factor2').value);
@@ -39,13 +35,11 @@ const multiplyNumbers = () => {
 document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
 /* Open Function Use - Divide Numbers */
 const divide = (number1, number2) => number1 / number2;
-
 const divideNumbers = () => {
     let divideNumber1 = Number(document.querySelector('#dividend').value);
     let divideNumber2 = Number(document.querySelector('#divisor').value);
     document.querySelector('#quotient').value = divide(divideNumber1, divideNumber2);
 }
-
 document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
 
 /* Decision Structure */
